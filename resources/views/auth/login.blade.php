30564d12b75fc2aee33c3429e34fec2636370d55
unknown <germandg99@gmail.com>
2019-05-18 22:28:02 -0500

    Vista de login y traduccion al espaniol

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,44 @@
 @extends('layouts.app')
+	
+@section('title', 'Iniciar sesion')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <div class="wrapper">
+        <div class="register-background"> 
+            <div class="filter-black"></div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1 mx-auto">
+                            <div class="register-card">
+                                <h3 class="title">Bienvenido!</h3>
+                                <form class="register-form" method="POST">
+                                    @csrf
+                                    <label>Correo electronico</label>
+                                    
+                                    <input name="email" required type="email" class="form-control" placeholder="Email">
+                                    @if ($errors->has('email'))
+                                        <span class="help-block">
+                                                <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                        <br>
+                                    @endif
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                                    <label>Contrasena</label>
+                                    <input name="password" min="6" required type="password" class="form-control" placeholder="Password">
+                                    @if ($errors->has('password'))
+                                        <span class="help-block">
+                                                <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                                    @endif
+                                    <button class="btn btn-danger btn-block">Iniciar sesion</button>
+                                </form>
+                                <div class="forgot">
+                                    <a href="#" class="btn btn-simple btn-danger">Olvide mi contrasena</a>
                                 </div>
                             </div>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+                    </div>
+                </div>     
         </div>
-    </div>
-</div>
+    </div>      
 @endsection
